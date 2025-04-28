8dbcf0f8a23a90e39c79bec19214625f60246d7e
mariapatrycja <149483478+MariaPatrycia@users.noreply.github.com>
2025-04-28 22:11:53 +0200

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,10 @@
 const create = async () => {
     // Write your code here 
-};
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');};
+     const fileExists = await fs.promises.access(filePath, fs.constants.F_OK).then(() => true).catch(() => false);
 
-await create();
+    if (fileExists) {
+        throw new Error('FS operation failed');
+    }
+    await fs.promises.writeFile(filePath, 'I am fresh and young');
+};await create();
