762603c67f6c36bf7a476b4b7d3dc0457097b025
mariapatrycja <149483478+MariaPatrycia@users.noreply.github.com>
2025-04-28 22:22:24 +0200

    Update delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,10 @@
 const remove = async () => {
     // Write your code here 
-};
-
+  const sourcePath = path.join(__dirname, 'wrongFilename.txt');
+  const targetPath = path.join(__dirname, 'properFilename.md');};
+ try {
+    await fs.promises.access(sourceFilePath, fs.constants.F_OK);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 await remove();
