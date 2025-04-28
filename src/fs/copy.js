6dc7b01a2be934973fc2a3d7dab394898caed9db
mariapatrycja <149483478+MariaPatrycia@users.noreply.github.com>
2025-04-28 22:17:31 +0200

    Update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,7 @@
 const copy = async () => {
     // Write your code here 
+    const sourceDir = path.join(__dirname, 'files');
+  const destDir = path.join(__dirname, 'files_copy');
 };
 
 await copy();
