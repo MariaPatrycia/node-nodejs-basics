ca43093d1f3fab38a79c5deb0edfedcdc4b2f60d
mariapatrycja <149483478+MariaPatrycia@users.noreply.github.com>
2025-04-28 22:26:57 +0200

    Update list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
 const list = async () => {
     // Write your code here 
-};
+    const list= async () => {
+  const dirPath = path.join(__dirname, 'files');
+
+  try {
+    await fs.promises.access(dirPath, fs.constants.F_OK);
+  } catch {
+    throw new Error('FS operation failed');
+  }};
 
 await list();
